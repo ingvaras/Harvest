4e4c255e66c06d3ad4e742a433861f84b7922708
Ingvaras Galinskas <ingvaraz4@gmail.com>
2017-08-28 19:32:13 +0300

    push again

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class Controls : MonoBehaviour {
 
 	private GameObject player;
+	public float speed;
 	private bool left;
 	private bool right;
 
 	void Start() {
+		speed = 5.0f;
 		left = false;
 		right = false;
 	}
 
 	void Update() {
 		if(left)
-			transform.position = Vector2.MoveTowards (transform.position, new Vector2(transform.position.x - 1, transform.position.y), Time.deltaTime);
+			transform.position = Vector2.MoveTowards (transform.position, new Vector2(transform.position.x - 1, transform.position.y), speed * Time.deltaTime);
 		if(right)
-			transform.position = Vector2.MoveTowards (transform.position, new Vector2(transform.position.x + 1, transform.position.y), Time.deltaTime);
+			transform.position = Vector2.MoveTowards (transform.position, new Vector2(transform.position.x + 1, transform.position.y), speed * Time.deltaTime);
 	}
 		
 	public void MoveLeft() {
