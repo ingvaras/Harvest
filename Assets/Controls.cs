f88464c2493d9f400058c5708926b609db09e589
Ingvaras Galinskas <ingvaraz4@gmail.com>
2017-08-28 19:10:28 +0300

    push

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class Controls : MonoBehaviour {
 
 	private GameObject player;
+	private bool left;
+	private bool right;
+
+	void Start() {
+		left = false;
+		right = false;
+	}
+
+	void Update() {
+		if(left)
+			transform.position = Vector2.MoveTowards (transform.position, new Vector2(transform.position.x - 1, transform.position.y), Time.deltaTime);
+		if(right)
+			transform.position = Vector2.MoveTowards (transform.position, new Vector2(transform.position.x + 1, transform.position.y), Time.deltaTime);
+	}
 		
 	public void MoveLeft() {
-		transform.position = Vector2.MoveTowards (transform.position, new Vector2(transform.position.x -1, transform.position.y), 1);
+		left = true;
 	}
 
 	public void MoveRight() {
-		transform.position = Vector2.MoveTowards (transform.position, new Vector2(transform.position.x + 1, transform.position.y), 1);
+		right = true;
+	}
+	
+	public void StopMoving() {
+		left = false;
+		right = false;
 	}
 }
