32a06f5cff26a5f8ddcd630ab9a7e0f2f65ab369
Paulius50 <www.paulius50@gmail.com>
2017-08-28 19:15:49 +0300

    v4

--- Assets/Scripts/Pig.cs
+++ Assets/Scripts/Pig.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class Pig : MonoBehaviour {
 	public Transform target;
+	private Vector3 targetpos;
 	// Use this for initialization
 	void Start () {
-		target = GameObject.FindGameObjectWithTag("Player").transform;
-
-
+		targetpos = GameObject.FindGameObjectWithTag("Player").transform.position;
+		angle ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = Vector2.MoveTowards (transform.position, target.position, 0.1f);
-		Vector3 vectorToTarget = target.position - transform.position;
+		
+		if (transform.position != targetpos) {
+			transform.position = Vector2.MoveTowards (transform.position, targetpos, 0.1f);
+		} else {
+			targetpos = new Vector3 (transform.position.x, transform.position.y - 10, 0);
+			angle ();
+
+		}
+
+	}
+
+	void angle(){
+		
+		Vector3 vectorToTarget = targetpos - transform.position;
 		float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg)+90;
 		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-		Debug.Log (q);
-		transform.rotation = Quaternion.Slerp(transform.rotation, q, 0.1f);
+		transform.rotation = Quaternion.Slerp(transform.rotation, q, 10f);
+
 	}
-		
 }
