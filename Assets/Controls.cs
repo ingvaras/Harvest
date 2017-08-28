4f61fba12df8ceee98b80745c03cab2f5799f196
Ingvaras Galinskas <ingvaraz4@gmail.com>
2017-08-28 18:44:12 +0300

    as

---
+++ Assets/Controls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+	private GameObject player;
+		
+	public void MoveLeft() {
+		transform.position = Vector2.MoveTowards (transform.position, new Vector2(transform.position.x -1, transform.position.y), 1);
+	}
+
+	public void MoveRight() {
+		transform.position = Vector2.MoveTowards (transform.position, new Vector2(transform.position.x + 1, transform.position.y), 1);
+	}
+}
