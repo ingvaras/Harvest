73ad9479d7be0d8122b60ce5690d9f609c440fd1
Paulius50 <www.paulius50@gmail.com>
2017-08-29 20:46:35 +0300

    v8

---
+++ Assets/Scripts/Hay_Bay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hay_Bay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (GameObject.FindGameObjectWithTag("Player") == null)
+			Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position -= new Vector3 (0, 0.1f,0);
+		if (transform.position.y < Camera.main.ScreenToWorldPoint (new Vector3 (0, 0, 0)).y)
+			Destroy (this.gameObject);
+	}
+}
